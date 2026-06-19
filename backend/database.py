ac72ef39b4356a6e3a6b2edd8878d800230523f6
nolawimelese <nolawimelese@gmail.com>
2026-06-18 23:47:57 -0400

    added database.py and database schema for track and strategies dbs

---
+++ backend/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+URL_DATABASE = "sqlite:///./mariokart.db"
+
+engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(declarative_base()):
+    pass
---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Track(Base):
+    __tablename__ = "tracks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    image_url = Column(String, index=True)
+    strategies = relationship("Strategy", back_populates="track")
+
+class Strategy(Base):
+    __tablename__ = "strategies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    track_id = Column(Integer, ForeignKey("tracks.id"), nullable=False)
+    name = Column(String, nullable=False)
+    position_min = Column(Integer, nullable=False)
+    position_max = Column(Integer, nullable=False)
+    track = relationship("Track", back_populates="strategies")
